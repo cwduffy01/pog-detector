280cb0bb491ccf8207e1856caff93edeab145819
msappington11 <71204360+msappington11@users.noreply.github.com>
2021-02-06 17:26:26 -0600

    Add files via upload

--- hackathon.py
+++ hackathon.py
@@ -5,7 +5,7 @@ import keyboard
 import time
 from PIL import Image
 
-faceCascade = cv2.CascadeClassifier(r'C:\Users\msapp\Desktop\Winning hackathon\haarcascade_frontalface_default.xml')
+faceCascade = cv2.CascadeClassifier(r'C:\Users\Scot\Desktop\Test\haarcascade_frontalface_default.xml')
 SCREEN_SIZE = (1920, 1080)
 fourcc = cv2.VideoWriter_fourcc(*"XVID")
 cap = cv2.VideoCapture(0)
@@ -14,24 +14,38 @@ def record():
     #creates variables
     temp = []
     FPS = 1000
-    duration = 10
+    duration = 15
     blackArray = []
     pogCount = 0
     
+    readyToRecord = False
+    stopRecord = 0
+    framesAfterPog = 0
+    
     while(True):
         start = time.time()
-        #takes picture of screen
-        img = pyautogui.screenshot()
-        frame = np.array(img)
-        #convert colors from BGR to RGB
+        
+        #takes picture of screen in Image type, converts the colors, and turns it back into an image
+        frame = pyautogui.screenshot()
+        frame = np.array(frame)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        #deletes the first image if necessary
-        temp.append(frame)
+        framePic = Image.fromarray(frame)
+        
+        #takes picture of face cam as np array and converts to image
+        ret, img = cap.read()
+        imagePic = Image.fromarray(img)
+        imagePic = imagePic.resize((int(len(img[0])/2), int(len(img)/2)))
+        
+        #puts face cam in the top left of the screen cap
+        framePic.paste(imagePic)
+        overlaid = np.asarray(framePic)
+        
+        #adds the new overlaid frame and deletes the first image if necessary
+        temp.append(overlaid)
         if(len(temp) > FPS * duration):
             temp.pop(0)
         
-        #Face recording========================================================
-        ret, img = cap.read()
+        #Face recognition==========================================================================
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = faceCascade.detectMultiScale(
             gray,     
@@ -58,14 +72,11 @@ def record():
             #convert back to array
             mouth = np.asarray(cropped)
             mouth = cv2.cvtColor(mouth, cv2.COLOR_BGR2GRAY)
-            #Image.fromarray(mouth).show()
             
             #detect black pixels
-            blackCount = 1;
-            for i in range(len(mouth)):
-                for a in range(len(mouth[0])):
-                    if(mouth[i][a] < 25):
-                        blackCount += 1
+            ret, black = cv2.threshold(mouth, 25, 255, cv2.THRESH_BINARY_INV)
+            cv2.imshow("mouth", black)
+            blackCount = np.sum(black == 255)
                         
             #makes sure there are 10 sample frames to start with
             if(len(blackArray) < 10):
@@ -100,11 +111,21 @@ def record():
         if(1/(end-start) < FPS):
             FPS = 1/(end-start)
             
-        #records the past 10 seconds once pogging
-        if(pogCount > 10):
+        #if the past 10 recorded frames have been pogging, checks that its ready
+        if(pogCount > 10):     
+            readyToRecord = True
+        
+        #continues to record for an aditional 5 seconds after the pog has been first detected
+        if(readyToRecord and framesAfterPog < FPS*5 - 10):
+            framesAfterPog += 1
+        #if it has been 5 seconds since the intital pog, it saves the video
+        elif(readyToRecord):
+            readyToRecord = False
+            framesAfterPog = 0
             vid = cv2.VideoWriter("output.mp4", fourcc, FPS, (SCREEN_SIZE))
             for frame in temp:
                 vid.write(frame)
+            print('Releasing video')
             vid.release()
             
         #REPLACE WITH CLICKING STOP BUTTON
@@ -115,4 +136,4 @@ def record():
     
 
 record()
-    
+
